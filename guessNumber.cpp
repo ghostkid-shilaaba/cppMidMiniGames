104a39161dff1443ea324d5360320d92529545e2
ghostkid-shilaaba <saadjilal8@gmail.com>
2026-02-12 17:10:29 +0100

    Update guessNumber.cpp

--- guessNumber.cpp
+++ guessNumber.cpp
@@ -7,34 +7,34 @@ int main() {
 	srand(time(0));
 	char replay{ 'y' };
 	while (replay == 'y') {
-		cout<< "\033[2J\033[1;1H";
+		cout << "\033[2J\033[1;1H";
 		int diffLvl{};
 		int maxRange{};
 		int nb{};
 		int tries{};
 		cout << "choose a difficulty\n1 for easy\t 2 for medium\t 3 for hard\n";
-		do {
-			cin >> diffLvl;
-			switch (diffLvl) {
-			case 1:
-				maxRange = 10;
-				nb = (rand() % maxRange) + 1;
-				tries = 3;
-				break;
-			case 2:
-				maxRange = 50;
-				nb = (rand() % maxRange) + 1;
-				tries = 5;
-				break;
-			case 3:
-				maxRange = 100;
-				nb = (rand() % maxRange) + 1;
-				tries = 7;
-				break;
-			default:
-				cout << "enter a nb between 1-3\n";
-			}
-		} while (diffLvl < 1 || diffLvl>3);
+		while (!(cin >> diffLvl)|| diffLvl>3||diffLvl<1) {
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "enter a nb between 1 and 3\n";
+		}
+		switch (diffLvl) {
+		case 1:
+			maxRange = 10;
+			nb = (rand() % maxRange) + 1;
+			tries = 3;
+			break;
+		case 2:
+			maxRange = 50;
+			nb = (rand() % maxRange) + 1;
+			tries = 5;
+			break;
+		case 3:
+			maxRange = 100;
+			nb = (rand() % maxRange) + 1;
+			tries = 7;
+			break;
+		}
 		cout << "guess a number between 1-" << maxRange << "\n";
 		while (tries > 0) {
 			int luckNb{};
@@ -57,8 +57,12 @@ int main() {
 			}
 		}
 		cout << "wanna replay?\n";
-		cin >> replay;
+		while (!(cin >> replay)) {
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "enter something valid\n";
+		}
 		replay = tolower(replay);
 	}
-	
+
 }
